f4b12771e7c25cec4d5552fd5cf14a8361b746ba
TshrXD <tushartarsariya12567@gmail.com>
2023-07-01 16:50:14 +0530

    chnage database

--- env.php
+++ env.php
@@ -1,9 +1,9 @@
 <?php
 
 $db_host = "localhost";
-$db_username = "tybca138";
-$db_password = "IL82";
-$db_name = "tybca138";
+$db_username = "root";
+$db_password = "";
+$db_name = "db_jobonline";
 
 $db_connect = mysqli_connect($db_host,$db_username,$db_password,$db_name) or die("Database connection error.");
 ?>
